5e078d6f4dbe9312e763800fda9808c2f5650afa
saurabhs5151-debug <saurabhs5151@gmail.com>
2026-07-16 03:58:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,18 @@
-import requests
-import time
-import os  # os को इम्पोर्ट करना जरूरी है ताकि Railway के variables पढ़ सकें
+import os
+import telebot
 
-# यहाँ हम कोड को बता रहे हैं कि डेटा Railway के Variables से लेना है
-TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+# Railway के Variables से टोकन और चैट आईडी ले रहे हैं
+BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 
-URL = f'https://api.telegram.org/bot{TOKEN}/'
+bot = telebot.TeleBot(BOT_TOKEN)
 
-while True:
-    try:
-        res = requests.get('https://api.delta.exchange/v2/tickers')
-        if res.status_code == 200:
-            data = res.json().get('result', [])
-            
-            btc = next((i for i in data if i.get('symbol') == 'BTCUSDT'), None)
-            eth = next((i for i in data if i.get('symbol') == 'ETHUSDT'), None)
-            
-            if btc and eth:
-                msg = f'BTC: ${btc.get("close")} | ETH: ${eth.get("close")}'
-                requests.post(URL + 'sendMessage', json={'chat_id': CHAT_ID, 'text': msg})
-    except:
-        pass
-    
-    time.sleep(15)
+# यह फंक्शन बोट को चालू करने के लिए जरूरी है
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "बोट चालू है और काम कर रहा है!")
+
+# यह लूप बोट को लगातार चालू रखता है
+if __name__ == "__main__":
+    print("बोट शुरू हो रहा है...")
+    bot.infinity_polling()
